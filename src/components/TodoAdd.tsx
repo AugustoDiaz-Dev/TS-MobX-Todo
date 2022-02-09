8de08a24f46f7ad7cb0d45bbcf359177f3acfe9d
Augusto <augustodiaz_1@hotmail.com>
2022-02-10 02:28:15 +1000

    TS-MobX

---
+++ src/components/TodoAdd.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Button, Input, Grid } from "@chakra-ui/react";
+import { observer } from 'mobx-react';
+
+import store from "../store";
+
+function TodoAdd() {
+  return (
+    <Grid pt={2} templateColumns="5fr 1fr" columnGap="3">
+      <Input placeholder="New todo"
+        value={store.newTodo}
+        onChange={(evt: any) => (store.newTodo = evt.target.value)}
+      />
+      <Button
+        onClick={() => store.addTodo()}
+      >Add Todo</Button>
+    </Grid>
+  );
+}
+
+export default observer(TodoAdd);
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
+import { observer } from 'mobx-react';
+
+import store, { Todo } from "../store";
+
+function TodoListItems() {
+  return (
+    <>
+      {store.todos.map((todo: Todo) => (
+        <Flex pt={2} key={todo.id}>
+          <Checkbox
+            onClick={() => todo.done = !todo.done}
+          />
+          <Input mx={2}
+            value={todo.text}
+            onChange={(evt: any) => (todo.text = evt.target.value)}
+          />
+          <Button
+            onClick={() => store.removeTodo(todo.id)}
+          >Delete</Button>
+        </Flex>
+      ))}
+    </>
+  );
+}
+
+const TodoListItemsObserver = observer(TodoListItems);
+
+function TodoList() {
+  return (
+    <>
+      <Heading>Todo List</Heading>
+      <TodoListItemsObserver />
+    </>
+  );
+}
+
+export default TodoList;
